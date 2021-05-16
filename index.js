995ace8de0f607bf2c4520ec05710726640bd107
yungestdev <80168738+yungestdev@users.noreply.github.com>
2021-05-16 17:45:46 +0200

    updat

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 const browser  = alert("This Website Uses Cookies only to start correctly, no dangerous cookies are allowed")
-const tutorial = alert("open .txt files and paste the link into your browser")
+function tutorial(){
+    const tutorial = alert("open .txt files and paste the link into your browser")
+}
